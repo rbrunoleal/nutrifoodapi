ca4e1cd58409faf23c9ec2d8a0f2e60be293ae15
Bruno Leal <robertobruno.leal@hotmail.com>
2018-12-04 15:41:06 +0000

    Ajustes Query

--- app/controllers/alimentos_controller.rb
+++ app/controllers/alimentos_controller.rb
@@ -10,29 +10,27 @@ class AlimentosController < ApplicationController
 	end
 	
 	def categoria
-		#categoria = Alimento.select('categoria')
 		categoria = Alimento.select(:categoria).map(&:categoria).uniq
     	render json: {status: 'SUCCESS', message:'Categorias:', data:categoria},status: :ok
 	end
 	
 	def regiao
-		#regiao = Alimento.select('regiao')
 		regiao = Alimento.select(:regiao).map(&:regiao).uniq
     	render json: {status: 'SUCCESS', message:'Regioes:', data:regiao},status: :ok
 	end
 	
 	def regiao_especifica
-		regiao = Alimento.find_by(regiao: params[:regiao])
-    	render json: {status: 'SUCCESS', message:'Alimentos Regiao:', data:regiao},status: :ok
+		regiao_esp = Alimento.where("regiao = :regiao", { regiao: params[:regiao] })
+    	render json: {status: 'SUCCESS', message:'Alimentos Regiao:', data:regiao_esp},status: :ok
 	end
 	
 	def categoria_especifica
-		categoria = Alimento.find_by(categoria: params[:categoria])
-    	render json: {status: 'SUCCESS', message:'Alimentos Categoria:', data:categoria},status: :ok
+		categoria_esp = Alimento.where("categoria = :categoria", { categoria: params[:categoria] })
+    	render json: {status: 'SUCCESS', message:'Alimentos Categoria:', data:categoria_esp},status: :ok
 	end
 	
 	def categoria_regiao_especifica
-		categoria_regiao = Alimento.find_by(categoria: params[:categoria], regiao: params[:regiao])
+		categoria_regiao = Alimento.where("categoria = :categoria and regiao = :regiao", { categoria: params[:categoria], regiao: params[:regiao] })
     	render json: {status: 'SUCCESS', message:'Alimentos Categoria Regiao:', data:categoria_regiao},status: :ok
 	end
 end
