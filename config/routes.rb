e50e4aad0eb605209d5c1549e60c7dd02aee762e
Bruno Leal <robertobruno.leal@hotmail.com>
2018-12-09 01:11:44 +0000

    Ajustes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  root "alimentos#index"
+  
   resources :alimentos, only: [:index, :show]
   get '/categorias', to: 'alimentos#categoria'
   get '/regioes', to: 'alimentos#regiao'
