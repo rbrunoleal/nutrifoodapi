1e83ba75a329621e89d8baeb35d85cc998305795
Bruno Leal <robertobruno.leal@hotmail.com>
2020-05-20 18:42:32 -0300

    Ajuste

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.serve_static_assets = true
+  config.serve_static_files = true
   config.public_file_server.enabled = true
 
   # Code is not reloaded between requests.
@@ -22,7 +23,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
